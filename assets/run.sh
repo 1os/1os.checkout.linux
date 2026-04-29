f05416f892f38c9a10f1a509c250d0f960b3db20
lazareveugene <lazareveugene@gmail.com>
2026-04-29 14:53:00 +0500

    2026.04.29.2

--- assets/run.sh
+++ assets/run.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 cd $SNAP/
+# ATOL .NET wrapper asks for "fptr10.dll" on Linux.
+# Ensure compatible aliases exist for the ELF library.
+if [ -f "./libfptr10.so" ]; then
+  [ -e "./fptr10.dll" ] || ln -s "./libfptr10.so" "./fptr10.dll" 2>/dev/null || cp "./libfptr10.so" "./fptr10.dll"
+  [ -e "./libfptr10.dll" ] || ln -s "./libfptr10.so" "./libfptr10.dll" 2>/dev/null || cp "./libfptr10.so" "./libfptr10.dll"
+  [ -e "./libfptr10.dll.so" ] || ln -s "./libfptr10.so" "./libfptr10.dll.so" 2>/dev/null || cp "./libfptr10.so" "./libfptr10.dll.so"
+fi
 ./1os.checkout start
